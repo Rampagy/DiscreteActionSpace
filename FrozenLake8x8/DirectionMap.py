5325154b0e158489854c6a10f70ef55942d572d6
Rampagy <ajsimon@mtu.edu>
2018-09-02 13:15:09 -0500

    improved comments

--- FrozenLake8x8/DirectionMap.py
+++ FrozenLake8x8/DirectionMap.py
@@ -19,10 +19,13 @@ NO_D = 0
 
 '''
 INDEX MAP
-0   1   2   3
-4   5   6   7
-8   9   10  11
-12  13  14  15
+0   1   2   3   4   5   6   7
+8   9   10  11  12  13  14  15
+16  17  18  19  20  21  22  23
+24  25  26  27  28  29  30  31
+I'm not typing the rest of this
+it just goes till it hits '63'.
+You get the idea.
 '''
 
 
